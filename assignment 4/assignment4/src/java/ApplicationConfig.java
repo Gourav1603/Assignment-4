132790f42675c3120b36cd8a031c994f89d7b8e7
c0617418 <c0617418@SB-N208-07.lambton.on.ca>
2015-07-09 15:56:29 -0400

    final commit

---
+++ assignment 4/assignment4/src/java/ApplicationConfig.java
@@ -0,0 +1,29 @@
+
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c0617418
+ */
+@javax.ws.rs.ApplicationPath("restfull")
+public class ApplicationConfig extends Application {
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+}
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+         resources.add(serveletdemo.class);
+    }
+}
